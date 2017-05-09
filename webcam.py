05e97813800e25b957c49fed90ff753c2928f7b6
arash parnia <arashparnia@gmail.com>
2017-05-09 14:41:25 +0200

    face detection working

--- webcam.py
+++ webcam.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+
+import calendar
 import time
 import requests
 import cv2
@@ -10,6 +12,77 @@ import json
 import urllib2
 import requests
 from requests.auth import HTTPBasicAuth
+
+
+# michel idx = 17 working stever
+# aart idx = 16 chaotic chriss
+
+#
+# headers = {'Authorization': 'Basic QXJhc2g6MTIzNDU='}
+#
+# request = urllib2.Request("http://jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=16&switchcmd=On",
+#                           headers=headers)
+#
+# contents = urllib2.urlopen(request).read()
+
+
+
+
+
+
+def recognized_chaotic_chris_on():
+    headers = {'Authorization': 'Basic QXJhc2g6MTIzNDU='}
+
+    request = urllib2.Request(
+        "http://jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=16&switchcmd=On",
+        headers=headers)
+
+    contents = urllib2.urlopen(request).read()
+
+
+def recognized_chaotic_chris_off():
+    headers = {'Authorization': 'Basic QXJhc2g6MTIzNDU='}
+
+    request = urllib2.Request(
+        "http://jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=16&switchcmd=Off",
+        headers=headers)
+
+    contents = urllib2.urlopen(request).read()
+
+
+def recognized_working_steve_on():
+    headers = {'Authorization': 'Basic QXJhc2g6MTIzNDU='}
+
+    request = urllib2.Request(
+        "http://jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=17&switchcmd=On",
+        headers=headers)
+
+    contents = urllib2.urlopen(request).read()
+
+
+def recognized_working_steve_off():
+    headers = {'Authorization': 'Basic QXJhc2g6MTIzNDU='}
+
+    request = urllib2.Request(
+        "http://jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=17&switchcmd=Off",
+        headers=headers)
+
+    contents = urllib2.urlopen(request).read()
+
+
+recognized_chaotic_chris_off()
+recognized_working_steve_off()
+
+
+# direct telegram message
+# https://api.telegram.org/bot353028451:AAEoXtjebnEbxWLJORYLGhTyie_co3u0HRo/sendmessage?chat_id=277127047&text=test
+
+# print(urllib.urlopen('http://jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=1&switchcmd=On').read())
+# urllib2.urlopen('http://arash:12345@jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=1&switchcmd=On')
+# urllib2.urlopen('http://arash:12345@jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=1&switchcmd=Off')
+# http://arash:12345@jebo.mynetgear.com:8080/json.htm?type=command&param=switchlight&idx=1&switchcmd=On
+
+
 
 
 def match_face(webcam_image):
@@ -27,17 +100,10 @@ def match_face(webcam_image):
 
     # print(CF.face_list.create('face_list_1', name='face_list_1', user_data=None))
 
-    # print(CF.face_list.add_face(image='images/', face_list_id='face_list_1'))
+    # print(CF.face_list.add_face(image='aart.jpg', face_list_id='face_list_1'))
 
     result_webcam = CF.face.detect(webcam_image, landmarks=False, attributes='age,gender')
 
-    # result_arash = CF.face.detect('images/arash.jpg', attributes='age,gender')
-    #
-    # result1 = CF.face.detect('images/f1.jpeg', attributes='age,gender')
-    # result2 = CF.face.detect('images/f2.jpg', attributes='age,gender')
-    # result3 = CF.face.detect('images/f3.jpg', attributes='age,gender')
-    # result4 = CF.face.detect('images/f4.jpg', attributes='age,gender')
-    # result5 = CF.face.detect('images/f5.jpg', attributes='age,gender')
 
     print(result_webcam)
 
@@ -61,8 +127,10 @@ def match_face(webcam_image):
             result_face = ('arash identified')
         elif (matchedId == '3d554532-d389-47f8-890b-6ff343058285'):
             result_face = ('michel identified')
-            # elif (matchedId == faceid4):
-            #     result_face = ('face 4 identified')
+            recognized_working_steve()
+        elif (matchedId == 'f09299d7-518d-4390-9245-7794933960f2'):
+            result_face = ('aart identified')
+            recognized_chaotic_chris()
             # elif (matchedId == faceid5):
             #     result_face = ('face 5 identified')
 
@@ -72,7 +140,8 @@ def match_face(webcam_image):
 casc_path = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(casc_path)
 face_detected = False
-while not face_detected:
+# while not face_detected:
+while True:
     video_capture = cv2.VideoCapture(0)
 
     # Capture frame-by-frame
@@ -89,24 +158,53 @@ while not face_detected:
         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
-    # Draw a rectangle around the faces
+    # recognizing face
+    if divmod(calendar.timegm(time.gmtime()), 20) == 0:
+        for (x, y, w, h) in faces:
+            cv2.imwrite('the_face.jpg', the_face)
+            print("Face Detected")
+            face_detected = True
+        print("processing face")
+        result_face = match_face('the_face.jpg')
+        print(result_face)
 
+
+
+
+    # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         the_face = frame[y:y + h, x:x + w]
-        cv2.imwrite('the_face.jpg', the_face)
-        print("Face Detected")
-        face_detected = True
+
+    cv2.imshow("frame", frame)
+
+    import threading
+
+    # def ():
+    #     threading.Timer(5.0, printit).start()
+    #     print
+    #     "Hello, World!"
+    #
+    #
+    # printit()
+
+
+    # recognized_chaotic_chris_off()
+    # recognized_working_steve_off()
+
+
+
+    # time.sleep(15)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 
 video_capture.release()
 cv2.destroyAllWindows()
 
-print("processing face")
-result_face = match_face('the_face.jpg')
-print(result_face)
 
 
-# cv2.imshow("cropped", theFace)
+
 
 # Display the resulting frame
 
