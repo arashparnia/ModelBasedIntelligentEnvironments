7801babaaf5d1b773ec1bbdaa8c27c32708c2b84
arash parnia <arashparnia@gmail.com>
2017-05-09 18:14:51 +0200

    face detection working

--- rl.py
+++ rl.py
@@ -1,5 +1,64 @@
 # https://github.com/NathanEpstein/reinforce
 
+import random
+import bisect
+import collections
+
+
+def cdf(weights):
+    total = sum(weights)
+    result = []
+    cumsum = 0
+    for w in weights:
+        cumsum += w
+        result.append(cumsum / total)
+    return result
+
+
+def choice(population, weights):
+    assert len(population) == len(weights)
+    cdf_vals = cdf(weights)
+    x = random.random()
+    idx = bisect.bisect(cdf_vals, x)
+    return population[idx]
+
+
+weights = [0.6, 0.5, 0.5, 0.9, 0.4]
+population = '12345'
+counts = collections.defaultdict(int)
+for i in range(100):
+    counts[choice(population, weights)] += 1
+
+import operator
+
+x = counts
+sorted_x = sorted(x.items(), key=operator.itemgetter(1), reverse=True)
+print(sorted_x)
+
+# % test.py
+# defaultdict(<type 'int'>, {'A': 3066, 'C': 2964, 'B': 3970})
+
+
+
+
+
+
+exit(0)
+
+
+def weighted_choice(choices):
+    total = sum(w for c, w in choices)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+    assert False, "Shouldn't get here"
+
+
+
+
 from reinforce.learn import MarkovAgent
 
 observations = [
